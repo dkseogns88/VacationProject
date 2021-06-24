3d54cb0501d35ad9c5df58dce2ddf706cb32c3a2
dkseogns88 <dkseogns88@naver.com>
2021-06-24 21:44:15 +0900

    second commit

--- VacationProject/main.cpp
+++ VacationProject/main.cpp
@@ -4,21 +4,21 @@ using namespace sf;
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(500, 500), "WINDOW");
-
-	RectangleShape rs; //사각형 오브젝트생성
-
-	//Vector2f = float 자료형 두개 짜리
-	rs.setSize(Vector2f(100.f, 100.f)); 
-	rs.setFillColor(Color::Green);
-	rs.setPosition(Vector2f(250.f, 250.f)); //생성위치 100.f , 100.f 
 	
-	rs.setOrigin(rs.getSize() / 2.f); //오브젝트의 중심을 set하는 함수
+	Sprite sp; //절대사이즈가 없다. scaling 필요
+	Texture tx; //texture의 사이즈를 그대로 가져오기에 배수에 곱해서 줄이거나 늘리는게 가능
+	tx.loadFromFile("Texture/20180109_143508.jpg");
+	sp.setScale(.100f,.100f);
+	sp.setOrigin(tx.getSize().x/2.f ,tx.getSize().y/2.f);
+	sp.setTexture(tx);
+	sp.setPosition(250.f, 250.f);
+	
+	
 
 	while (window.isOpen())//윈도우가 오픈되어있을때
 	{
 		window.clear();
-		//rs.setPosition(rs.getPosition().x + .01f, rs.getPosition().y + .01f);
-		window.draw(rs); //rs오브젝트를 윈도우에 그림
+		window.draw(sp);
 		window.display(); //그린결과물을 화면으로출력 
 	}
 }
